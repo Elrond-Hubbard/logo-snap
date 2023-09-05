43cf1a0e33519d28c296c64e9e0b5b0090fe6cbe
Elrond-Hubbard <136862666+Elrond-Hubbard@users.noreply.github.com>
2023-09-05 14:35:03 -0400

    corrected module path

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const fs = require('fs');
-const shapes = require('./lib/shapes')
-const svg = require('./lib/svg')
+const {Shape, Square, Triangle, Circle} = require('./lib/shapes')
+const SVG = require('./lib/svg')
+
+
+
+const circle = new Circle();
+circle.setColor("green")
+circle.render();
 
 
 
